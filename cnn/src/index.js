1c41afbd0957ac773d65e76b9cb15cb239b478e5
massyao <massyao@126.com>
2019-07-01 16:54:06 +0800

    clean code
    clean code

--- cnn/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './app/main/main';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Main />, document.getElementById('root'));
-registerServiceWorker();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Main from './app/main/main';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+registerServiceWorker();
+
+/*
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,3 +17,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
