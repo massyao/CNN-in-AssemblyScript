a22bc46a9f33ead1431d7d5d0f371c31c30432d1
massyao <massyao@126.com>
2019-07-02 09:21:58 +0800

    fix error
    fix error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './app/main/main';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+serviceWorker.unregister();
+
+
+/*
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
 
-/*
+
+
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
